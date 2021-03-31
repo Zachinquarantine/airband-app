18741451bfd98874a2e2121784fd1df6f0120a99
Zachinquarantine <69423184+Zachinquarantine@users.noreply.github.com>
2021-03-30 22:30:44 -0400

    imports dark theme module

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { DefaultTheme, DarkTheme } from “@react-navigation/native”;
 
 export default function App() {
   return (
